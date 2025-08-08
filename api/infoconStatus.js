03d354301ef01b303aac45774dabb602fa64525f
sug4r-wr41th <sug4r_wr41th@proton.me>
2025-08-08 14:50:46 +0200

    update: INFOCON status API

--- api/infoconStatus.js
+++ api/infoconStatus.js
@@ -2,10 +2,22 @@ export default async function handler(req, res) {
   try {
     const response = await fetch("https://isc.sans.edu/infocon.txt");
 
-    const data = await response.text();
+    let status = await response.text();
+
+    let color = "";
+
+    switch ( status ) {
+      case "green": color = "#51B435"; break;
+      case "yellow": color ="#F2F250"; break;
+      case "orange": color = "#F6C844"; break;
+      case "red": color = "#D72F20"; break;
+      default: color = "#2D25E1"; break;
+    }
+
+    const data = { status, color }
 
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Content-Type", "application/text");
+    res.setHeader("Content-Type", "application/json");
     res.status(200).send(data);
   } catch (e) {
     res.status(500).send("Error:: URL not fetched");
