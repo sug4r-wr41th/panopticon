918142c889fcd2833fd60cf28c4efe30e7d54f1f
sug4r-wr41th <sug4r_wr41th@proton.me>
2025-08-10 09:47:34 +0200

    add: RSS feed proxy API

---
+++ api/rss.js
@@ -0,0 +1,18 @@
+export default async function handler(req, res) {
+  const url = res.query.url;
+
+  if (!url) { res.status(500).send("error::query_missing_URL"); }
+
+  try {
+    const response = await fetch(url);
+
+    const data = await response.text()
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Vary", "Origin");
+    res.setHeader("Content-Type", "application/xml");
+    res.status(200).send(data);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+}
